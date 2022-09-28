4a0a007e749f3c2d194117b39d51daf2381c313b
Biraj <birajkarki9849@gmail.com>
2022-09-28 12:16:21 +0545

    initail commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    }
+};
+
+export const youtubeOptions = {
+    method: 'GET',
+    params: { id: 'UCE_M8A5yxnLfW0KghEeajjw' },
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+
+    return data;
+};
